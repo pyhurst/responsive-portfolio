c611718c04072f1272d9221201de720187e0e4e6
Philip Hurst <youngphil9@gmail.com>
2021-02-09 14:12:37 -0600

    added scrollreveal to js, header and buttons scroll to divs

---
+++ assets/main.js
@@ -0,0 +1,19 @@
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '80px',
+    duration: 2000,
+    reset: true
+})
+
+sr.reveal('.title', {})
+sr.reveal('.home_buttons', {delay: 200})
+sr.reveal('.social-icon', {interval: 200})
+
+sr.reveal('.about-img', {})
+sr.reveal('.about-main', {delay: 200})
+sr.reveal('.about-quote', {interval: 200})
+
+sr.reveal('.card', {delay: 200})
+
+sr.reveal('.form-control', {delay: 200})
+sr.reveal('.info', {inteval: 200})
